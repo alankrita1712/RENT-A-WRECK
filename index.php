3139538cb18230a8c1f96485854739413f3dec97
alankrita1712 <30411855+alankrita1712@users.noreply.github.com>
2019-07-20 21:35:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title> project </title>
+<script src = "javascriptfunctions.js"></script>
+</head>
+<body style="background-color:powderblue;">
+	
+<hr>
+
+	<div id = AdHocSection class = "section">
+	<img src="72_convertible-car.jpg" width="1500" height="555">	
+	<h1 style="text-align:center;">RENT_A_WRECK</h1>
+	<h4 style="text-align:center;">ENTER QUERY</h4>
+	<form action = "runQuery.php" method="post" target = "_blank">
+		<strong>Enter your query below: </strong> <br/> 
+		<textarea name="queryBox" rows="10" cols="100" id = "queryBox"></textarea><br/>
+		<input class = "textBoxButton" type = "submit">
+	</form>
+	</div>
+	<hr>
+	</body>
+</html>
